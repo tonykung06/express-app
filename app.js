c77a8cf256b3eaa515004f30baa63bb1bf9c8a3b
tonykung <tonykung06@gmail.com>
2015-12-13 20:44:22 +0800

    handle request

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express();
 
 var port = 5000;
 
+app.get('/*', function(req, res, next) {
+	res.send('nodejs app here...');
+});
+
 app.listen(port, function(err) {
 	if (!err) {
 		console.log('running express server on port ' + port);
