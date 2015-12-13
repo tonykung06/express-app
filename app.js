97ab84c89cab70f6bded219a88340c1186d04f79
tonykung <tonykung06@gmail.com>
2015-12-13 22:43:26 +0800

    add npm script, bootstrap tempalte

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ app.disable('x-powered-by');
 
 var port = 5000;
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
 app.get('/', function(req, res, next) {
-	res.send('nodejs app here...');
+	res.send('Hello World');
+});
+
+app.get('/books', function(req, res, next) {
+	res.send('Hello Books');
 });
 
 app.listen(port, function(err) {
