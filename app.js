ec2625913e559f2d0cec45606cd3af23b9260362
tonykung <tonykung06@gmail.com>
2015-12-15 22:29:11 +0800

    add handlebars templating engine

--- app.js
+++ app.js
@@ -8,10 +8,17 @@ var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.set('views', './src/views');
+
+var handlebars = require('express-handlebars');
+app.engine('hbs', handlebars({
+	extname: 'hbs'
+}));
+
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res, next) {
-    res.render('index', {
+    res.render('index.hbs', {
+    	title: 'hihihi',
     	list: ['2', '3']
     });
 });
